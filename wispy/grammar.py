179b08f2c9ef4c02e44bea38b5e112b14ba904da
Alexandru Coman <mail@alexcoman.com>
2014-08-31 16:19:03 +0300

    Add grammar for variables.

--- wispy/grammar.py
+++ wispy/grammar.py
@@ -10,7 +10,7 @@
 
 from modgrammar import (
     Grammar, OR, WORD, REPEAT, ANY_EXCEPT,
-    OPTIONAL, WHITESPACE,
+    OPTIONAL, WHITESPACE, ANY
 )
 
 
@@ -113,3 +113,70 @@ class RealLiteral(Grammar):
 
         (DecimalDigits, ExponentPart, OPTIONAL(DecimalTypeSuffix),
          OPTIONAL(NumericMultiplier)))
+
+
+# Variables
+class VariableCharacter(Grammar):
+    # pylint: disable = too-many-function-args
+
+    grammar = OR(
+        WORD("A-Z", max=1),  # Letter, Uppercase
+        WORD("a-z", max=1),  # Letter, Lowercase
+        # Letter, Titlecase
+        WORD("\u01C5", "\u01C8", "\u01CB", "\u01F2", "\u1F88", "\u1F89",
+             "\u1F8A", "\u1F8B", "\u1F8C", "\u1F8D", "\u1F8E", "\u1F8F",
+             "\u1F98", "\u1F99", "\u1F9A", "\u1F9B", "\u1F9C", "\u1F9D",
+             "\u1F9E", "\u1F9F", "\u1FA8", "\u1FA9", "\u1FAA", "\u1FAB",
+             "\u1FAC", "\u1FAD", "\u1FAE", "\u1FAF", "\u1FBC", "\u1FCC",
+             "\u1FFC", max=1),
+        # Letter, Modifier
+        WORD("\u02B0-\u02EE", max=1),
+        WORD("\u0374", "\u037A", "\u0559", "\u0640", "\u06E5", "\u06E6",
+             "\u07F4", "\u07F5", "\u07FA", "\u081A", "\u0824", "\u0828"),
+        # TODO: Add more characters from the 'Letter, Modifier` Category
+        # TODO: Add characters from the 'Letter, Other' Category
+
+        # Number, Decimal Digit
+        WORD("0-9"),
+        WORD("\u0660-\u0669"),  # ARABIC-INDIC DIGIT
+        WORD("\u06F0-\u06F9"),  # EXTENDED ARABIC-INDIC DIGIT
+        # TODO: Add more haracter from the 'Number, Decimal Digit' Category
+        WORD("\u005F"),         # The underscore character
+        WORD("?")
+    )
+
+
+class VariableCharacters(Grammar):
+    grammar = OR(
+        VariableCharacter,
+        (VariableCharacters, VariableCharacter)
+    )
+
+
+class VariableNamespace(Grammar):
+    grammar = (VariableCharacters, ":")
+
+
+class VariableScope(Grammar):
+    grammar = OR("global:", "local:", "private:", "script:",
+                 VariableNamespace)
+
+
+class EscapedCharacter(Grammar):
+    grammar = ("\u0060", ANY)
+
+
+class BracedVariableCharacter(Grammar):
+    grammar = OR(ANY_EXCEPT("\u007D\u0060", max=1), EscapedCharacter)
+
+
+class BracedVariableCharacters(Grammar):
+    grammar = OR(
+        (BracedVariableCharacter),
+        (BracedVariableCharacters, BracedVariableCharacter)
+    )
+
+
+class BracedVariable(Grammar):
+    grammar = ("$", "{", OPTIONAL(VariableScope),
+               BracedVariableCharacters, "}")
