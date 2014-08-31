5583d360dd5fde5537f5df8446153bfde2e72fa4
Alexandru Coman <mail@alexcoman.com>
2014-08-31 16:50:26 +0300

    Fix for .

--- wispy/grammar.py
+++ wispy/grammar.py
@@ -118,40 +118,41 @@ class RealLiteral(Grammar):
 
 # Variables
 class VariableCharacter(Grammar):
-    # pylint: disable = too-many-function-args
-
     grammar = OR(
         WORD("A-Z", max=1),  # Letter, Uppercase
         WORD("a-z", max=1),  # Letter, Lowercase
+
         # Letter, Titlecase
-        WORD("\u01C5", "\u01C8", "\u01CB", "\u01F2", "\u1F88", "\u1F89",
-             "\u1F8A", "\u1F8B", "\u1F8C", "\u1F8D", "\u1F8E", "\u1F8F",
-             "\u1F98", "\u1F99", "\u1F9A", "\u1F9B", "\u1F9C", "\u1F9D",
-             "\u1F9E", "\u1F9F", "\u1FA8", "\u1FA9", "\u1FAA", "\u1FAB",
-             "\u1FAC", "\u1FAD", "\u1FAE", "\u1FAF", "\u1FBC", "\u1FCC",
-             "\u1FFC", max=1),
+        WORD("\u01C5"), WORD("\u01C8"), WORD("\u01CB"), WORD("\u01F2"),
+        WORD("\u1F88"), WORD("\u1F89"), WORD("\u1F8A"), WORD("\u1F8B"),
+        WORD("\u1F8C"), WORD("\u1F8D"), WORD("\u1F8E"), WORD("\u1F8F"),
+        WORD("\u1F98"), WORD("\u1F99"), WORD("\u1F9A"), WORD("\u1F9B"),
+        WORD("\u1F9C"), WORD("\u1F9D"), WORD("\u1F9E"), WORD("\u1F9F"),
+        WORD("\u1FA8"), WORD("\u1FA9"), WORD("\u1FAA"), WORD("\u1FAB"),
+        WORD("\u1FAC"), WORD("\u1FAD"), WORD("\u1FAE"), WORD("\u1FAF"),
+        WORD("\u1FBC"), WORD("\u1FCC"), WORD("\u1FFC"),
+
         # Letter, Modifier
         WORD("\u02B0-\u02EE", max=1),
-        WORD("\u0374", "\u037A", "\u0559", "\u0640", "\u06E5", "\u06E6",
-             "\u07F4", "\u07F5", "\u07FA", "\u081A", "\u0824", "\u0828"),
+        WORD("\u0374"), WORD("\u037A"), WORD("\u0559"), WORD("\u0640"),
+        WORD("\u06E5"), WORD("\u06E6"), WORD("\u07F4"), WORD("\u07F5"),
+        WORD("\u07FA"), WORD("\u081A"), WORD("\u0824"), WORD("\u0828"),
         # TODO: Add more characters from the 'Letter, Modifier` Category
         # TODO: Add characters from the 'Letter, Other' Category
 
         # Number, Decimal Digit
-        WORD("0-9"),
-        WORD("\u0660-\u0669"),  # ARABIC-INDIC DIGIT
-        WORD("\u06F0-\u06F9"),  # EXTENDED ARABIC-INDIC DIGIT
+        WORD("0-9", max=1),
+        WORD("\u0660-\u0669", max=1),  # ARABIC-INDIC DIGIT
+        WORD("\u06F0-\u06F9", max=1),  # EXTENDED ARABIC-INDIC DIGIT
         # TODO: Add more character from the 'Number, Decimal Digit' Category
-        WORD("\u005F"),         # The underscore character
+
+        WORD("\u005F"),                # The underscore character
         WORD("?")
     )
 
 
 class VariableCharacters(Grammar):
-    grammar = OR(
-        VariableCharacter,
-        (VariableCharacters, VariableCharacter)
-    )
+    grammar = REPEAT(VariableCharacter)
 
 
 class VariableNamespace(Grammar):
