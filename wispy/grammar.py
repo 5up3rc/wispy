dfaf50111df03c3ffe321b60c826ecc99c099a68
Claudiu Popa <claudiu@ropython.org>
2014-08-31 10:28:34 +0300

    PEP8 fixes.

--- wispy/grammar.py
+++ wispy/grammar.py
@@ -11,18 +11,23 @@ from modgrammar import (
     OPTIONAL, WHITESPACE,
 )
 
+
 class Newline(Grammar):
     grammar = OR("\n", "\r", "\r\n")
+
 
 class InputCharacter(Grammar):
     grammar = ANY_EXCEPT("\r\n", max=1)
 
+
 class InputCharacters(Grammar):
     grammar = REPEAT(InputCharacter)
+
 
 class SingleLineComment(Grammar):
     # todo: stops at '# '
     grammar = ("#", OPTIONAL(WHITESPACE), OPTIONAL(InputCharacters))
+
 
 class Keyword(Grammar):
     # TODO: catches `if` in `ifuntil`.
@@ -35,34 +40,44 @@ class Keyword(Grammar):
                  "switch", "throw", "trap", "try",
                  "until", "using", "var", "while")
 
+
 class HexadecimalDigit(Grammar):
     grammar = WORD('0-9a-fA-F', max=1)
+
 
 class HexadecimalDigits(Grammar):
     grammar = REPEAT(HexadecimalDigit)
 
+
 class LongTypeSuffix(Grammar):
     grammar = WORD("l", max=1)
+
 
 class DecimalTypeSuffix(Grammar):
     grammar = WORD("d", max=1)
 
+
 class NumericTypeSuffix(Grammar):
     grammar = OR(DecimalTypeSuffix, LongTypeSuffix)
 
+
 class NumericMultiplier(Grammar):
     grammar = REPEAT(OR("kb", "mb", "tb", "pb", "gb"), max=1)
+
 
 class HexadecimalIntegerLiteral(Grammar):
     grammar = ("0x", HexadecimalDigits,
                OPTIONAL(LongTypeSuffix),
                OPTIONAL(NumericMultiplier))
 
+
 class DecimalDigit(Grammar):
     grammar = WORD('0-9', max=1)
 
+
 class DecimalDigits(Grammar):
     grammar = REPEAT(DecimalDigit)
+
 
 class DecimalIntegerLiteral(Grammar):
     grammar = (DecimalDigits,
