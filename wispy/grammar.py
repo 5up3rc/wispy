365aac7febff07df5fb09b8e0b52565903339f8b
Alexandru Coman <mail@alexcoman.com>
2014-09-03 22:32:49 +0300

    Add a couple of missing docstrings.

--- wispy/grammar.py
+++ wispy/grammar.py
@@ -1258,6 +1258,30 @@ class PostDecrementExpression(Grammar):
 
 
 class ElementAccess(Grammar):
+
+    """This operator is left associative.
+
+    Examples:
+
+    ::
+
+    $a = [int[]](10,20,30)          # [int[]], Length 3
+    $a[1]                           # returns int 20
+    $a[20]                          # no such position, returns $null
+    $a[-1]                          # returns int 30, i.e., $a[$a.Length-1]
+    $a[2] = 5                       # changes int 30 to int 5
+    $a[20] = 5                      # implementation-defined behavior
+    $a = New-Object 'double[,]' 3,2
+    $a[0,0] = 10.5                  # changes 0.0 to 10.5
+    $a[0,0]++                       # changes 10.5 to 10.6
+    $list = ("red",$true,10),20,(1.2, "yes")
+    $list[2][1]                     # returns string "yes"
+    $a = @{ A = 10 },@{ B = $true },@{ C = 123.45 }
+    $a[1]["B"]                      # $a[1] is a Hashtable, where B is a key
+    $a = "red","green"
+    $a[1][4]                        # returns string "n" from string in $a[1]
+    """
+
     grammar = (PrimaryExpression, "[",
                OPTIONAL(NewLines), Expression, OPTIONAL(NewLines), "]")
 
@@ -1399,10 +1423,67 @@ class MemberName(Grammar):
 
 
 class MemberAccess(Grammar):
+
+    """The operator . is used to select an instance member from an object,
+    or a key from a Hashtable.
+    The left operand must designate an object, and the right operand must
+    designate an accessible instance member.
+
+    Examples:
+
+    ::
+    $a = 10,20,30
+    $a.Length                           # get instance property
+    (10,20,30).Length
+    $property = "Length"
+    $a.$property                        # property name is a variable
+    $h1 = @{ FirstName = "James"; LastName = "Anderson"; IDNum = 123 }
+    $h1.FirstName                       # designates the key FirstName
+    $h1.Keys                            # gets the collection of keys
+    [int]::MinValue                     # get static property
+    [double]::PositiveInfinity          # get static property
+    $property = "MinValue"
+    [long]::$property                   # property name is a variable
+    foreach ($t in [byte],[int],[long])
+    {
+        $t::MaxValue                   # get static property
+    }
+    $a = @{ID=1},@{ID=2},@{ID=3}
+    $a.ID                              # get ID from each element in the array
+    """
+
     grammar = (PrimaryExpression, OR(".", "::"), MemberName)
 
 
 class InvocationExpression(Grammar):
+
+    """An :class InvocationExpression: calls the method designated by
+        * :class PrimaryExpression: . :class MemberName:
+        * :class PrimaryExpression: :: :class MemberName:
+
+    The parentheses in argument-list contain a possibly empty, comma-separated
+    list of expressions, which designate the arguments whose values are passed
+    to the method.
+
+    Examples:
+
+    ::
+        [math]::Sqrt(2.0)               # call method with argument 2.0
+        [char]::IsUpper("a")            # call method
+        $b = "abc#$%XYZabc"
+        $b.ToUpper()                    # call instance method
+        [math]::Sqrt(2)                 # convert 2 to 2.0 and call method
+        [math]::Sqrt(2D)                # convert 2D to 2.0 and call method
+        [math]::Sqrt($true)             # convert $true to 1.0 and call method
+        [math]::Sqrt("20")              # convert "20" to 20 and call method
+        $a = [math]::Sqrt               # get method descriptor for Sqrt
+        $a.Invoke(2.0)                  # call Sqrt via the descriptor
+        $a = [math]::("Sq"+"rt")        # get method descriptor for Sqrt
+        $a.Invoke(2.0)                  # call Sqrt via the descriptor
+        $a = [char]::ToLower            # get method descriptor for ToLower
+        $a.Invoke("X")                  # call ToLower via the descriptor
+    """
+
     grammar = (
         PrimaryExpression, OR(".", "::"), MemberName,
         ArgumentList
