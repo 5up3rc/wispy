64d30ef376583ee2100f773243d1e0e7d89d0cff
Claudiu Popa <claudiu@ropython.org>
2014-09-07 23:17:27 +0300

    Remove obsolete test.

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -264,8 +264,7 @@ class GrammarTest(unittest.TestCase):
             ";",
             "process { }",
             "\n[test[]]\nparam\n($var\n$var\n)"
-            ";"
-            "process { }"
+            ";",
         ]
         self._test_expected(ScriptBlock, literals)
 
